015a5e8579ec8793df3a892f93fbbb7344531415
10comclique <10comclique@gmail.com>
2026-05-31 16:21:36 -0300

    projeto pague junto inicial

---
+++ public/firebase-config.js
@@ -0,0 +1,12 @@
+// Configuração do Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyDwq-s-7v3bX77TlAspAIGw0B90bvyI5Vk",
+  authDomain: "pague-junto-web.firebaseapp.com",
+  projectId: "pague-junto-web",
+  storageBucket: "pague-junto-web.firebasestorage.app",
+  messagingSenderId: "443381652655",
+  appId: "1:443381652655:web:7dc35c589de2b29919885a",
+  measurementId: "G-7RK19M4WKJ"
+};
+// Exportar para as outras telas usarem
+export default firebaseConfig;
